d413e18819b11855d9c660074f4cdd7a92803e9f
Austin Luo <austin.luo06@gmail.com>
2015-08-02 17:21:17 -0400

    Added solution to euler 1

---
+++ euler1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+	int sum = 0;
+	for ( int i = 1; i < 1000; i++ ) {
+		if ( i % 3 == 0 ) {
+			sum += i;
+		}
+		else if ( i % 5 == 0 ) {
+			sum += i;
+		}
+	}
+	std::cout << sum << std::endl;
+}
