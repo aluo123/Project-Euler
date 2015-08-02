6687753bb905cc7872c159edcb90d816331b2f0b
Austin Luo <austin.luo06@gmail.com>
2015-08-02 18:01:12 -0400

    Added solution to euler 3

---
+++ euler3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+long largestPrimeFactor( long num ) {
+	int factor = 2;
+	//If it finds a factor, divides the number by that factor. When the factor reaches the number,
+	//the number will be a prime because we've removed all other factors ( including any smaller prime factors )
+	while ( factor < num ) {
+		if ( num % factor == 0 ) {
+			num /= factor;
+			factor = 2;
+		}
+		else {
+			factor++;
+		}
+	}
+	return factor;
+}
+
+int main(){
+	std::cout << largestPrimeFactor(600851475143) << std::endl;
+}
