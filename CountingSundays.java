c2b9a4a6c01b61850678e5a9af55c94bd0659191
Austin Luo <austin.luo06@gmail.com>
2015-11-15 13:15:04 -0500

    Delete CountingSundays.java
    Deleted wrong file counting sundays code

--- CountingSundays.java
+++
@@ -1,9 +0,0 @@
-//Project Euler #19
-//Count all the sundays from Jan 1, 1901 and Dec 31, 2000
-
-public class CountingSundays {
-    public static void main( String[] args ) {
-        int x = - 1;
-    }
-}
-        
